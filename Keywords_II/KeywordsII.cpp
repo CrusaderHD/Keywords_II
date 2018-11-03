c9de599ea472268446177708d79f47a93a95211b
Hunter Derrick <Hunderri@uat.edu>
2018-11-02 23:51:07 -0700

    Added in 2 extra enums and 20 extra words to guess. Cycles through each word and has player guess 3 times prior to restart. Still needs soome polish, but works.

--- Keywords_II/KeywordsII.cpp
+++ Keywords_II/KeywordsII.cpp
@@ -5,7 +5,7 @@
  * Assignment: KeywordsII
  * Class: CSC215 @ UAT
  * Project Start date: 10/29/2018
- * Project Complete date: 
+ * Project Complete date: 11/02/2018
  */
 //Task:
 /*
@@ -57,14 +57,13 @@ using namespace std;
 */
 
 
-
-int main()
+//Function to greet the user.
+void GreetUser()
 {
-
 	bool playAgain = false;  //Setting a bool to determine if Player wants to play more than once.
+	
 	string userName;
 	string startGameWord;
-
 
 	// Display Title of program to user
 	//Let's welcome the player to our Game.
@@ -82,7 +81,7 @@ int main()
 	cout << "\n" << userName << " KeywordsII is a decoder game. \nYou will be given a word to Decode.\nIt will be your duty as a new recruit to decode the word.\n";
 	cout << userName << " are you up for the challenge? Yes or No?" << endl;
 	cin >> startGameWord;
-	if(startGameWord == "Yes" || startGameWord == "yes")
+	if (startGameWord == "Yes" || startGameWord == "yes")
 	{
 		cout << "\nGood Choice " << userName << " not like I would have given you one anyway.\n" << endl;
 	}
@@ -91,7 +90,7 @@ int main()
 		cout << "\nOops, you didn't type yes. That's fine though you are playing anyway. You're a recruit remember....\n\n";
 	}
 
-	
+
 	// Display an directions to the recruit on how to use Keywords
 	cout << "I guess I better tell you how to play." << endl;
 	cout << userName << ", the concept is easy. Just re-read what I already told you to do earlier." << endl;
@@ -110,9 +109,16 @@ int main()
 		cout << "\nOops, you haven't learned yet. You don't have a choice. You're a recruit remember....\n\n";
 	}
 
+}
 
+//Function for main game play.
+void PlayKeywords()
+{
+	string userName;
+	string startGameWord;
+	bool playAgain = false;
 
-	//Create a Do_While Loop. This will run the game until player decides they no longer wish to play.
+	//Create a Do_While Loop. This will run the game until player decides they no longer wish to play. Player has to guess all three words 3 times prior to being prompt to play again.
 	do
 	{
 		//For_Loop. This will run through 3 random words. Player guesses 3 words for each game before being asked to play again.
@@ -120,11 +126,10 @@ int main()
 		{
 			bool playAgain = false;
 
-			enum fields { WORD, HINT, NUM_FIELDS };
-			//State how many words there are.
-			const int NUM_WORDS = 10;
-			//Initialize Words and corresponding hints. Word on left, Hint on Right.
-			const string WORDS[NUM_WORDS][NUM_FIELDS] =
+			enum fields { WORDONE, HINTONE, NUM_FIELDS_0 };
+
+			const int NUM_WORDS_0 = 10;  			//State how many words there are in the first iteration.
+			const string WORDS_0[NUM_WORDS_0][NUM_FIELDS_0] =
 			{
 				{"alpha", "First letter of Military Phonetic Alphabet"},
 				{"eagle", "Freedom Bird"},
@@ -137,42 +142,125 @@ int main()
 				{"television","You watch me. "},
 				{"keyboard","Odds are, you're using me right now. How dare you use me like that."}
 			};
-
 			//Picking a random word from the choices...
 			srand(static_cast<unsigned int>(time(0)));
-			int choice = (rand() % NUM_WORDS);
-			string theWord = WORDS[choice][WORD]; //The word to Guess...
-			string theHint = WORDS[choice][HINT]; //The hint to the word...
-
+			int choice_0 = (rand() % NUM_WORDS_0);
+			string theWord_0 = WORDS_0[choice_0][WORDONE]; //The word to Guess...
+			string theHint = WORDS_0[choice_0][HINTONE]; //The hint to the word...
 			//Now time to create a jumbled version of chosen word.
-			string randWordChoice = theWord;
+			string randWordChoice = theWord_0;
 			int length = randWordChoice.size();
 			for (int i = 0; i < length; ++i)
 			{
+				int index0 = (rand() % length);
 				int index1 = (rand() % length);
-				int index2 = (rand() % length);
-				char temp = randWordChoice[index1];
-				randWordChoice[index1] = randWordChoice[index2];
-				randWordChoice[index2] = temp;
+				char temp0 = randWordChoice[index0];
+				randWordChoice[index0] = randWordChoice[index1];
+				randWordChoice[index1] = temp0;
 			}
 
-			//Show player what the scrambled word is.
-			cout << "\nThe scrambled word is: " << randWordChoice;
+			//Create another enum for the second batch of words.
+			enum { WORDTWO, HINTTWO, NUM_FIELDS_1};
 
-			//Ask user to guess what the word is.
-			string guess;
-			cout << "\n\nWhat is your guess " << userName << "? ";
-			cin >> guess;
+			const int NUM_WORDS_1 = 10;  			//State how many words there are in the first iteration.
+			const string WORDS_1[NUM_WORDS_1][NUM_FIELDS_1] =
+			{
+				{"star", "It shines brightly at night."},
+				{"camera", "I take pictures or videos."},
+				{"phone", "I can talk, text, record and many other things."},
+				{"gold", "My nemesis is silver."},
+				{"stairs", "Up, Down? Just walk, I can take you both ways."},
+				{"brain", "You should use me when thinking. I am a useful tool."},
+				{"desk", "I typically have a computer on top of me."},
+				{"fan", "I spin in circles all day and help people be cool."},
+				{"controller", "You use me to play most video games."},
+				{"ground", "Odds are, you walk on me at least once a day."}
+			};
 
+			srand(static_cast<unsigned int>(time(0)));
+			int choice_1 = (rand() % NUM_WORDS_1);
+			string theWord_1 = WORDS_1[choice_1][WORDTWO];
+			string theHint_1 = WORDS_1[choice_1][WORDTWO];
+
+			string randWordChoice_1 = theWord_1;
+			int length_1 = randWordChoice_1.size();
+			for(int i = 0; i < length_1; ++i)
+			{
+				int index_2 = (rand() % length_1);
+				int index_3 = (rand() % length_1);
+				char temp_1 = randWordChoice_1[index_2];
+				randWordChoice_1[index_2] = randWordChoice_1[index_3];
+				randWordChoice_1[index_3] = temp_1;
+			}
+
+
+			//Create a third enum for the third batch of words.
+			enum { WORDTHREE, HINTTHREE, NUM_FIELDS_2 };
+
+			const int NUM_WORDS_2 = 10;  			//State how many words there are in the first iteration.
+			const string WORDS_2[NUM_WORDS_2][NUM_FIELDS_2] =
+			{
+				{"mario", "Its'a me _______. Also  a Nintendo Icon."},
+				{"luigi", "Green Brother. Also had a mansion where he hunted ghosts."},
+				{"square", "A more evenly sided rectangle."},
+				{"pokemon", "Gotta Catch'em all..."},
+				{"yoshi", "Always willing to help Mario. Even if it means his own demise."},
+				{"dragon", "I breathe hot fire."},
+				{"blanket", "When you are cold, you will use me."},
+				{"swimming", "I am something you do in a pool."},
+				{"vehicle", "Vroom Vroom."},
+				{"toilet", "Remember to flush..."}
+			};
+
+			srand(static_cast<unsigned int>(time(0)));
+			int choice_2 = (rand() % NUM_WORDS_2);
+			string theWord_2 = WORDS_2[choice_2][WORDTHREE];
+			string theHint_2 = WORDS_2[choice_2][WORDTHREE];
+
+			string randWordChoice_2 = theWord_2;
+			int length_2 = randWordChoice_2.size();
+			for (int i = 0; i < length_2; ++i)
+			{
+				int index_4 = (rand() % length_2);
+				int index_5 = (rand() % length_2);
+				char temp_2 = randWordChoice_2[index_4];
+				randWordChoice_2[index_4] = randWordChoice_2[index_5];
+				randWordChoice_2[index_5] = temp_2;
+			}
+
+
+			//Show player what the scrambled words are and take in their guesses. .
+			cout << "\nWord One: " << randWordChoice;
+			string guessOne;
+			cout << "\n\nWhat is your first guess " << userName << "? ";
+			cin >> guessOne;
+
+			cout << "\nWord Two: " << randWordChoice_1;
+			string guessTwo;
+			cout << "\nWhat is your second guess " << userName << "? ";
+			cin >> guessTwo;
+
+			cout << "\nWord Three: " << randWordChoice_2;
+			string guessThree;
+			cout << "\nWhat is your third guess " << userName << "? ";
+			cin >> guessThree;
 
 			//Entering the Game Loop
 			//While_Loop will run as long as the users guess does not equal 'The Word' or quit. Will also allow user to ask for a hint.
-			while ((guess != theWord) && (guess != "quit"))
+			while ((guessOne != theWord_0 || guessTwo != theWord_1 || guessThree !=  theWord_2) && (guessOne != "quit"))
 			{
 				//If user asks for a hint.
-				if (guess == "hint")
+				if (guessOne == "hint")
 				{
-					cout << "\nThe Hint is: " << theHint;
+					cout << "\nThe Hint for word one is: " << theHint;
+				}
+				else if (guessTwo == "hint")
+				{
+					cout << "\nThe Hint for word two is: " << theHint_1;
+				}
+				else if (guessThree == "hint")
+				{
+					cout << "\nThe Hint for word three is: " << theHint_2;
 				}
 				else
 				{
@@ -180,15 +268,37 @@ int main()
 				}
 				//After hint is displayed, ask user to guess.
 				cout << "\n\nWhat is your guess: ";
-				cin >> guess;
+				cin >> guessOne;
 			}
 
 			//If the user guesses the word.
-			if (guess == theWord)
+			if (guessOne == theWord_0)
 			{
-				cout << "\nCONGRATULATIONS. You guessed correctly.\n" << endl;
+				cout << "\nCONGRATULATIONS. You guessed word one correctly.\n" << endl;
 			}
-			else if (guess == "quit" || "Quit")
+			else if (guessOne == "quit" || "Quit")
+			{
+				cout << "I am sad to see you leave. But I understand. :( " << endl;
+				playAgain = false;
+				break;
+			}
+
+			if (guessTwo == theWord_1)
+			{
+				cout << "\nCONGRATULATIONS. You guessed word two correctly.\n" << endl;
+			}
+			else if (guessOne == "quit" || "Quit")
+			{
+				cout << "I am sad to see you leave. But I understand. :( " << endl;
+				playAgain = false;
+				break;
+			}
+
+			if (guessThree == theWord_2)
+			{
+				cout << "\nCONGRATULATIONS. You guessed word three correctly.\n" << endl;
+			}
+			else if (guessOne == "quit" || "Quit")
 			{
 				cout << "I am sad to see you leave. But I understand. :( " << endl;
 				playAgain = false;
@@ -213,8 +323,13 @@ int main()
 	} while (playAgain);
 
 	cout << "\nThanks for playing. Have a nice day!\n" << endl;
+}
+
+int main()
+{
+	GreetUser();
+	PlayKeywords();
 
 	system("pause");
 	return 0;
-
 }
